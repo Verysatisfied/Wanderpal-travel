eed8548c79f2cb87d63c2e1cd0014988dd59a604
Verysatisfied <lmuiiu@foxmail.com>
2024-03-28 15:32:35 +0000

    test limited function only to paid user

--- src/pages/ProtectedFeatureLink.js
+++ src/pages/ProtectedFeatureLink.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import { useEffect, useCallback } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
