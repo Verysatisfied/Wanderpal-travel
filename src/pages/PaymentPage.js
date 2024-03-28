25f722d5473d668c1944a20619b8123d9fdaf9e2
Verysatisfied <lmuiiu@foxmail.com>
2024-03-28 14:00:41 +0000

    test payment page

--- src/pages/PaymentPage.js
+++ src/pages/PaymentPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../assets/wrappers/Payment.css";
