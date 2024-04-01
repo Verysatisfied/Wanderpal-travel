33559fd523532fe9b47b2dea4e5ef459e3c8a434
Verysatisfied <lmuiiu@foxmail.com>
2024-04-01 02:42:17 +0100

    add weather warning

--- src/pages/dashboard/Weather.js
+++ src/pages/dashboard/Weather.js
@@ -1,25 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import CityComponent from "../../components/CityComponent";
 import WeatherComponent from "../../components/WeatherComponent";
-
-export const WeatherIcons = {
-  "01d": "../../assets/icons/icons/sunny.svg",
-  "01n": "../../assets/icons/night.svg",
-  "02d": "../../assets/icons/day.svg",
-  "02n": "../../assets/icons/cloudy-night.svg",
-  "03d": "../../assets/icons/cloudy.svg",
-  "03n": "../../assets/icons/cloudy.svg",
-  "04d": "../../assets/icons/perfect-day.svg",
-  "04n": "../../assets/icons/cloudy-night.svg",
-  "09d": "../../assets/icons/rain.svg",
-  "09n": "../../assets/icons/rain-night.svg",
-  "10d": "../../assets/icons/rain.svg",
-  "10n": "../../assets/icons/rain-night.svg",
-  "11d": "../../assets/icons/storm.svg",
-  "11n": "../../assets/icons/storm.svg",
-};
 
 const Container = styled.div`
   display: flex;
@@ -34,39 +19,35 @@ const Container = styled.div`
   font-family: Montserrat;
 `;
 
-const AppLabel = styled.span`
-  color: black;
-  margin: 20px auto;
-  font-size: 18px;
-  font-weight: bold;
-`;
-const CloseButton = styled.span`
-  padding: 2px 3px;
-  background-color: black;
-  border-radius: 50%;
-  color: white;
-  position: absolute;
-`;
-
 function Weather() {
-  const [city, updateCity] = useState();
-  const [weather, updateWeather] = useState();
+  const [city, updateCity] = useState("");
+  const [weather, updateWeather] = useState(null);
+
   const fetchWeather = async (e) => {
     e.preventDefault();
-    const response = await Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
-    );
-    updateWeather(response.data);
+    try {
+      const response = await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+      );
+      updateWeather(response.data);
+    } catch (error) {
+      toast.error("City not found. Please enter a valid city name.");
+    }
   };
+
+  const clearWeather = () => {
+    updateWeather(null);
+  };
+
   return (
     <Container>
-      {/* <AppLabel>React Weather App</AppLabel> */}
-      {city && weather ? (
-        <WeatherComponent weather={weather} city={city} />
+      {weather ? (
+        <WeatherComponent weather={weather} city={city} goBack={clearWeather} />
       ) : (
         <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
       )}
     </Container>
   );
 }
+
 export default Weather;
